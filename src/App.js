0ad5bcc4b9bcf801901e29a11dce85067045e140
Cihan Argan <cihan.argan@outlook.com>
2022-07-19 16:41:24 +0300

    React İle Component Oluşturma Navi,categoryList,ProductList

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import CategoryList from "./CategoryList";
+import Navi from "./Navi";
+import ProductList from "./ProductList";
 
 function App() {
   return (
     <div >
-      <h2>Hello from React !!! </h2>
+     <Navi/>
+     <CategoryList/>
+     <ProductList/>
     </div>
+   
   );
 }
 
---
+++ src/Navi.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Navi extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Navi Componentimiz Oluştu</h3>
+     </div>
+    )
+  }
+}
