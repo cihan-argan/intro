52d55624706e8591c0935942ce6c91aa39fe38bf
Cihan Argan <cihan.argan@outlook.com>
2022-07-21 15:04:19 +0300

    ReactStrap ve Bootstrap ile tasarim olusturma

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from "react";
+import { Container, Row, Col } from "reactstrap";
 import CategoryList from "./CategoryList";
 import Navi from "./Navi";
 import ProductList from "./ProductList";
 
 function App() {
   return (
-    <div >
-     <Navi/>
-     <CategoryList/>
-     <ProductList/>
+    <div>
+    <Container>
+      <Row>
+        <Navi />
+      </Row>
+      <Row>
+        <Col xs="3">
+          <CategoryList />
+        </Col>
+        <Col xs="9">
+          <ProductList />
+        </Col>
+      </Row>
+    </Container>
     </div>
-   
   );
 }
 
